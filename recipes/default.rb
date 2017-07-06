81effa12284d964188b005be908dd7336312d6d3
Alastair Holman <alastair.holman@skybettingandgaming.com>
2017-07-06 15:40:53 +0100

    fix variable name typo

--- recipes/default.rb
+++ recipes/default.rb
@@ -23,7 +23,7 @@ node['teagent']['package'].each do |pkg,ver|
                 action :upgrade
             else
                 action :install
-                version ver.to_s if vers.match(/^([0-9]+[.]){2,}[0-9]+-[1-9][0-9]*$/)
+                version ver.to_s if ver.to_s.match(/^([0-9]+[.]){2,}[0-9]+-[1-9][0-9]*$/)
             end
             allow_downgrade true
             notifies(:run, 'execute[config_teagent.sh]', :delayed) if pkg == 'te-agent'
